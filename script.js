d839c0ae277af8366bd6a2b1e602391bbb2dfce1
KriptoAzad <34945667+KriptoAzad@users.noreply.github.com>
2025-03-13 12:44:30 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function joinFuse() {
+    alert("Welcome to the Fuse Army! 🚀");
+}
+
+document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("You've joined the Fuse Army! 🎉");
+});
